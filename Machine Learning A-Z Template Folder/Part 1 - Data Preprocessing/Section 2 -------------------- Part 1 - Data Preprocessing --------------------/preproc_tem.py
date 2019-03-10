f34bf900715508209d16ef40f6f0445b4a5d0d37
werneckg <werneck.guilherme@gmail.com>
2019-03-09 21:45:36 -0300

    Template de preprocessamento de dados finalizado.

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/preproc_tem.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/preproc_tem.py
@@ -33,6 +33,15 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, 
                                                     random_state = 0)
 
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+
+
+
 
 
 
